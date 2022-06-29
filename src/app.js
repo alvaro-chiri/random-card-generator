a356f99ab3b45cf06cff3cfa4225c93d63d95fb5
alvaro-chiri <alvaro.chirinos@yahoo.com>
2022-06-29 00:06:31 +0000

    save

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let suits = ["♦", "♥", "♠", "♣"];
+  let randNum = Math.floor(Math.random() * numbers.length);
+  document.querySelector(".cardValue").innerText = numbers[randNum];
+  console.log(randNum);
+  let randFace = Math.floor(Math.random() * suits.length);
+  document.querySelector(".bottomFace").innerText = suits[randFace];
+  document.querySelector(".topFace").innerText = suits[randFace];
+  console.log(suits);
 };
